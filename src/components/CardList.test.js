689ca4c7342b168c0011dcb91a4b4862c991d231
Ryan Pedersen <ryanpedersen42@gmail.com>
2020-02-23 13:38:42 -0600

    break jest tests for demo

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import CardList from './CardList';
 const filteredRobots = [{
     id: 1,
-    name: 'Leanne Graham222',
+    name: 'Leanne Graham',
     username: 'Bret',
     email: 'Sincere@april.biz'
   }]
